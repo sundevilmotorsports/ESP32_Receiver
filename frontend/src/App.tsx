9f7c8e4a0abd13163582ede67ebd590b30c1d6eb
Nolan Peterson <ndpete2007@gmail.com>
2026-02-18 20:19:15 -0700

    switch preact to react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import {useTheme} from "../components/theme-provider.tsx";
+
+function App() {
+  return (
+      <div className="flex min-h-svh flex-col items-center justify-center">
+
+      </div>
+  )
+}
+
+export default App
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-	plugins: [preact()],
-	build: {
-		rollupOptions: {
-			output: {
-				entryFileNames: `assets/[name].js`,
-				chunkFileNames: `assets/[name].js`,
-				assetFileNames: `assets/[name].[ext]`,
-			},
-		},
-	},
-});
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
